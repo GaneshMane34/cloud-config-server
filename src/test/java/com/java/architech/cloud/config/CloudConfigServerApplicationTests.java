a99012bb3a0094c8c997261d197c94363f114d36
GaneshMane34 <ganeshmane2009@gmail.com>
2022-09-28 11:10:20 +0530

    Cloud Config Server : Initial commit

---
+++ src/test/java/com/java/architech/cloud/config/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.architech.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
